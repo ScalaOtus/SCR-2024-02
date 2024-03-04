28902383d64e9d20d2f5fd39d801896f3e5058e1
furiosaming <furiosaming@mail.ru>
2024-03-04 11:35:52 +0300

    add: methods sum, scale, axpy in LinearAlgebraOps

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,5 +1,21 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = ???
+  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length == v2.length) equalLengthSum(v1, v2)
+    else throw new Exception("Operation is not supported")
+  }
+
+  def scale(a: Int, v: Array[Int]): Array[Int] = {
+    v.map(v => v * a)
+  }
+
+  def axpy(a: Int, v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length == v2.length) equalLengthSum(v1.map(v1 => v1 * a), v2)
+    else throw new Exception("Operation is not supported")
+  }
+
+  private def equalLengthSum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    v1.zip(v2).map(v => v._1 + v._2)
+  }
 }
